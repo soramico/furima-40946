d193911475ff723ff9a890f125f2a5f8a4a2c721
soramico <soramiko.akiyama@gmail.com>
2024-10-18 01:30:02 +0900

    Revert "ユーザー認証機能の実装"
    This reverts commit 7db9b8a0106eb81858fa8a7ca8b8b3ed91514d92.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,12 +7,5 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
-  end
-
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
 end
