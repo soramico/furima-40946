6a27ce854591a606062da7ea155e454671dbf134
soramico <soramiko.akiyama@gmail.com>
2024-10-19 16:32:37 +0900

    モデルの単体テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+    end
+    it 'nicknameが空では登録できない' do
+    end
+    it 'emailが空では登録できない' do
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+    end
+    it '重複したemailが存在する場合は登録できない' do
+    end
+    it 'emailは@を含まないと登録できない' do
+    end
+    it 'passwordが5文字以下では登録できない' do
+    end
+    it 'passwordが129文字以上では登録できない' do
+    end
+    it 'first_nameが空では登録できない' do
+    end
+    it 'last_nameが空では登録できない' do
+    end
+    it 'first_name_kanaが空では登録できない' do
+    end
+    it 'last_name_kanaが空では登録できない' do
+    end
+    it 'birthdayが空では登録できない' do
+    end
+  end
 end
