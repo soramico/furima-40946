e69f924eb2227fdc1ebe1000b004b7b933896a55
soramico <soramiko.akiyama@gmail.com>
2024-10-25 01:58:28 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  # アクティブハッシュとのアソシエーション
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :contribution
+  belongs_to :prefecture
+  belongs_to :day
 end
