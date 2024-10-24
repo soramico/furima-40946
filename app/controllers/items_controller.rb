281956efeb2cf9f38a381274f3743544a631ec7b
soramico <soramiko.akiyama@gmail.com>
2024-10-25 01:19:50 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:product_name, :product_description, :category_id, :condition_id, :contribution_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
