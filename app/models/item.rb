a1a79d38ba3badf0466b81739ffe35701709192c
soramico <soramiko.akiyama@gmail.com>
2024-10-25 02:07:33 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one :order
   has_one_attached :image
@@ -9,4 +11,25 @@ class Item < ApplicationRecord
   belongs_to :contribution
   belongs_to :prefecture
   belongs_to :day
+
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :product_name 
+    validates :product_description
+    validates :category_id
+    validates :condition_id
+    validates :contribution_id
+    validates :prefecture_id
+    validates :day_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :condition_id
+      validates :contribution_id
+      validates :prefecture_id
+      validates :day_id
+    end
 end
