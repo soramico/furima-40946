142b89bd39e385071034cf7ed92cfc057a24669b
soramico <soramiko.akiyama@gmail.com>
2024-10-27 18:02:55 +0900

    レビュー修正_テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,6 +37,11 @@ RSpec.describe Item, type: :model do
     end
 
     context '出品ができないとき' do
+      it 'ユーザー情報がない場合は登録できないこと' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
       it '１枚画像がないと出品できない' do
         @item.image = nil
         @item.valid?
