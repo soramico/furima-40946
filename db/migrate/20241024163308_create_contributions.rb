df6452bd93f43e67d75a749a3786b6c46c981fef
soramico <soramiko.akiyama@gmail.com>
2024-11-10 18:05:19 +0900

    再デプロイエラー解消

--- db/migrate/20241024163308_create_contributions.rb
+++
@@ -1,8 +0,0 @@
-class CreateContributions < ActiveRecord::Migration[7.0]
-  def change
-    create_table :contributions do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20241027075901_rename_day_id_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameDayIdColumnToItems < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :items, :day_id, :deliveryday_id
-  end
-end
